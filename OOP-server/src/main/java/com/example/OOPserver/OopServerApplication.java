89210bb3868c43aaf7be339716ee1764bbf333a3
REV623 <90751501+REV623@users.noreply.github.com>
2023-03-26 00:20:25 +0700

    Add files via upload

---
+++ OOP-server/src/main/java/com/example/OOPserver/OopServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.OOPserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopServerApplication.class, args);
+	}
+
+}
---
+++ OOP-server/src/test/java/com/example/OOPserver/OopServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OOPserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
